2c4a4b9db50b4e4ef8b4555803c558fb890f147c
pschultz <pschultz@kpmg.com>
2019-04-06 14:07:25 -0400

    updated for covar

--- regression.go
+++ regression.go
@@ -7,6 +7,52 @@ import (
 )
 
 /*
+Covariance in Statistics: What is it? Example
+
+Statistics Definitions > Covariance
+
+Contents (Click to skip to that section):
+
+    Definition & Formula
+    Example
+    Problems with Interpretation
+    Advantages
+    Covariance in Excel
+
+Definition & Formula
+
+Covariance is a measure of how much two random variables vary together. It’s similar to variance, but where variance tells you how a single variable varies, co variance tells you how two variables vary together.
+Covariance
+
+Image from U of Wisconsin.
+
+The Covariance Formula
+
+The formula is:
+Cov(X,Y) = Σ E((X-μ)E(Y-ν)) / n-1 where:
+X is a random variable
+E(X) = μ is the expected value (the mean) of the random variable X and
+E(Y) = ν is the expected value (the mean) of the random variable Y
+n = the number of items in the data set
+Back to top
+Example
+
+
+
+Calculate covariance for the following data set:
+x: 2.1, 2.5, 3.6, 4.0 (mean = 3.1)
+y: 8, 10, 12, 14 (mean = 11)
+
+Substitute the values into the formula and solve:
+Cov(X,Y) = ΣE((X-μ)(Y-ν)) / n-1
+= (2.1-3.1)(8-11)+(2.5-3.1)(10-11)+(3.6-3.1)(12-11)+(4.0-3.1)(14-11) /(4-1)
+= (-1)(-3) + (-0.6)(-1)+(.5)(1)+(0.9)(3) / 3
+= 3 + 0.6 + .5 + 2.7 / 3
+= 6.8/3
+= 2.267
+*/
+
+/*
 Take in vectors: x, y
 Optimize for SumofSquares
 Compare w/ Py output
@@ -74,7 +120,7 @@ func ComputeVectorMean(x []float64) (float64, error) {
 //# calculate variance using a list comprehension
 //var_res = sum((xi - m) ** 2 for xi in results) / len(results)
 
-//ComputeVectorVariance returns vector of variances for x
+//ComputeVectorVariance returns variance for x
 func ComputeVectorVariance(x []float64) (float64, error) {
 	diffs := make([]float64, len(x))
 	meanx, _ := ComputeVectorMean(x)
@@ -99,32 +145,59 @@ func AddtoVector(x []float64, add float64) ([]float64, error) {
 
 //Compute the covariance between two vectors
 /*
-Cov(X,Y) = Σ E((X-μ)E(Y-ν)) / n-1 where:
+Cov(X,Y) = Σ
+E(
+	(X-μ)
+	E(Y-ν)
+	) / n-1 where:
 X is a random variable
 E(X) = μ is the expected value (the mean) of the random variable X and
 E(Y) = ν is the expected value (the mean) of the random variable Y
 n = the number of items in the data set
+
+
+from numpy
+sum items in m along colums
+>>> m -= np.sum(m * w, axis=1, keepdims=True) / v1
+multiply the summed items with transpose
+>>> cov = np.dot(m * w, m.T) * v1 / (v1**2 - ddof * v2)
+
+Calculate covariance for the following data set:
+x: 2.1, 2.5, 3.6, 4.0 (mean = 3.1)
+y: 8, 10, 12, 14 (mean = 11)
+
+Substitute the values into the formula and solve:
+Cov(X,Y) = ΣE((X-μ)(Y-ν)) / n-1
+= (2.1-3.1)(8-11)+(2.5-3.1)(10-11)+(3.6-3.1)(12-11)+(4.0-3.1)(14-11) /(4-1)
+= (-1)(-3) + (-0.6)(-1)+(.5)(1)+(0.9)(3) / 3
+= 3 + 0.6 + .5 + 2.7 / 3
+= 6.8/3
+= 2.267
+*/
+
+
 */
 
 func ComputeVectorCovariance(x, y []float64) (float64, error) {
+	// this is broken
+	// FIX IT
+	if len(x) != len(y) {
+		return nil, errors.New("ComputeVectorCovariance: input vectors are length %d and %d, needs to be ==", len(x), len(y))
+	}
 	meanx, _ := ComputeVectorMean(x)
 	meany, _ := ComputeVectorMean(y)
+	outvector := make([]float64, len(x))
 	//make vector of x-meanz, y-meany
-	diffx, _ := AddtoVector(x, -meanx)
-	diffy, _ := AddtoVector(y, -meany)
-	fmt.Println("diffx", diffx)
-	fmt.Println("diffy", diffy)
-	//get the mean
-	meanDiffy, _ := ComputeVectorMean(diffy)
-	meanDiffx, _ := ComputeVectorMean(diffx)
-	fmt.Println("mean diff x", meanDiffx)
-	fmt.Println("mean diff y", meanDiffy)
-	//The -1 part below we should make input
-	fmt.Println(meanDiffx * meanDiffy)
-	coVar := meanDiffx * meanDiffy / (float64(len(x)) - 1)
-	return coVar, nil
-
+	for ind, number := range x {
+		resx := number - meanx
+		resy := y[ind] - meany
+		outvector = append(resx * resy , outvector)
+	}
+	covar := SumVector(outvector) / len(outvector)
+	return covar, nil
 }
+
+
 
 //Regressor fits LinReg struct on input values x and output y
 func Regressor(x, y []float64) LinReg {
@@ -137,5 +210,4 @@ func Regressor(x, y []float64) LinReg {
 
 	//check weights for fit
 	return LinReg{1, []float64{1, 2, 3}}
-
 }
