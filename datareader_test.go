2ca36db98fb324be063d588101671856c9e3393d
patrickschu <patrickschultz@utexas.edu>
2019-04-07 18:58:18 -0400

    rm tests

--- datareader_test.go
+++ datareader_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestReadCsv(t *testing.T) {
 	sampleFile := "/Users/pschultz/Documents/golinreg/testframe.csv"
-	df, err := ReadCsvRows(sampleFile)
-	if err != nil {
-		t.Errorf("Error %d raised, want: raise", err)
-	}
-	fmt.Println(df)
+	//df, err := ReadCsvRows(sampleFile)
+	//if err != nil {
+	//	t.Errorf("Error %d raised, want: raise", err)
+	//}
+	fmt.Println(sampleFile)
 
 	//comparevectors()
 
