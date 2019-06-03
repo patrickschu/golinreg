8437a51202a1443896e442c180c4a7fe3643209a
pschultz <pschultz@kpmg.com>
2019-06-03 20:34:25 +0200

    rename datareader

---
+++ csvreader/csvreader.go
@@ -0,0 +1,58 @@
+//Data read functions
+package csvreader
+
+import (
+	"encoding/csv"
+	"io"
+	"os"
+)
+
+//ReadCsvRows reads file from `inputFilePath` and stores in map by row
+func ReadCsvRows(inputFilePath string) (map[int64][]string, error) {
+	// open file, give to `NewReader`
+	var ind int64
+	dataframe := make(map[int64][]string)
+
+	fileObject, err := os.Open(inputFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer fileObject.Close()
+	csvReader := csv.NewReader(fileObject)
+	for {
+		row, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		dataframe[ind] = row
+		ind++
+	}
+	return dataframe, nil
+}
+
+//readCsvCols reads file from `inputFilePath` and stores in map by col
+func ReadCsvCols(inputFilePath string) (map[int][]string, error) {
+	dataframe := make(map[int][]string)
+	fileObject, err := os.Open(inputFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer fileObject.Close()
+	csvReader := csv.NewReader(fileObject)
+	for {
+		row, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		for ind, item := range row {
+			dataframe[ind] = append(dataframe[ind], item)
+		}
+	}
+	return dataframe, nil
+}
